44d578937783ed275f9e8175d1f60c947f216963
Satish Singh <i.sksingh113@gmail.com>
2025-12-31 18:58:21 +0530

    final

--- authService/clearCookie.js
+++ authService/clearCookie.js
@@ -2,18 +2,18 @@ const { serialize } = require("cookie");
 const clearTokenCookie = (res) => {
   res.setHeader("Set-Cookie", [
     serialize("token", "", {
-      //  domain:".example.com",
+       domain:".example.com",
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       expires: new Date(0),
     }),
     serialize("token_middleware", "", {
-      //  domain:".example.com",
+       domain:".example.com",
       httpOnly: false,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       expires: new Date(0),
     }),
