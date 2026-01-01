6ad15a4dbe4d63a27ba86966ca64e8e0eebc127f
Md Zafar <mz5659875@gmail.com>
2026-01-01 20:36:20 +0530

    Updated Cookies Configs

--- authService/setTokenCookie.js
+++ authService/setTokenCookie.js
@@ -3,19 +3,19 @@ const setTokenCookie = (res, token, middlewareToken) => {
   res.setHeader("Set-Cookie", [
     // Secure, HttpOnly cookie
     serialize("token", token, {
-      // domain:".miorish.com",
+      domain:".miorish.com",
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       maxAge: 60 * 60 * 24 * 30, 
     }),
     // Non-HttpOnly for middleware
     serialize("token_middleware", middlewareToken, {
-      //  domain:".miorish.com",
+       domain:".miorish.com",
       httpOnly: false,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       path: "/",
       maxAge: 60 * 60 * 24 * 30,
     }),
